a3ecd0fdff9f0590a027cae89f8f09ed379f45ea
Jonathan Lee <jonathanleeee@gmail.com>
2017-11-28 11:38:55 -0800

    dynamically update li by entering text field

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,21 @@
-$("li").click(function(){
+$("ul").on('click', 'li', function(){
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete Todo
-$("span").click(function(event){
+$("ul").on('click', 'span', function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 })
+
+$("input[type='text']").keypress(function(){
+	if(event.which === 13){
+		// grabbing the text from input field
+		var todoText = $(this).val();
+		$(this).val("");
+		// create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
