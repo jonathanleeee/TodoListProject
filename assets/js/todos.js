ba0314ee19591566f57f504831cdb13da33b7476
Jonathan Lee <jonathanleeee@gmail.com>
2017-11-28 10:44:18 -0800

    now when span X is clicked, the entire li will fadeout and remove

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,11 @@
 $("li").click(function(){
 	$(this).toggleClass("completed");
 });
+
+//click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
